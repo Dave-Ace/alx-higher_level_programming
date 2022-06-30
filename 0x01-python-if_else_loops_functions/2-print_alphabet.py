bb0686582518fcfe2456964960e5449b9bb1c3ad
Toluwani <davidoyekanmi.t@gmail.com>
2022-06-30 00:08:15 +0000

    ascii characters

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    print("{:c}".format(char(i)), end='')
+    print("{:s}".format(chr(i)), end='')
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(65, 123):
+for i in range(97, 123):
     if chr(i) != 'p'and chr(i) != 'e':
         print("{:s}".format(chr(i)), end='')

3acc76129b636131f9d8959f7821cab3b0cbfc21
Toluwani <davidoyekanmi.t@gmail.com>
2022-07-02 09:33:19 +0000

    integer combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for x in range(i+1, 10):
-        if i == 9:
+        print("{}".format(i), end='')
+        print("{}".format(x), end=', ')
+        if i+1 == 9:
             print("\n")
-        else:
-            print("{}".format(i))
-            print("{}".format(x), end=', ')

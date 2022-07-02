f802e906616babfb290d30110ed098aa577040c7
Toluwani <davidoyekanmi.t@gmail.com>
2022-07-02 09:59:34 +0000

    converts lowercase to uppercase without str.upper()

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str)
+    for i in str:
+        if (ord(i) > 64 and ord(i) < 91) or i == ' ':
+            print("{s}".format(i))
+        else:
+            print("{s}".format(chr(ord(i) - 32)))
